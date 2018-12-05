b162f5ff2c4a0a86464a120a31cd2d2706e01624
Heather Thompson <heather.c.thompson@gov.bc.ca>
2018-12-05 15:32:36 -0800

    Data cleaning each parameter at each station.

--- 02_clean.R
+++ 02_clean.R
@@ -21,7 +21,59 @@ distinct(all_data_shuswap, SAMPLE_CLASS)
 shuswap_clean <- filter(shuswap_clean, SAMPLE_CLASS != "Blank - field")
 
 ## Refine dataframe (df) to include a subset of columns for simplified viewing analysis
-shuswap_df <- select(shuswap_clean, EMS_ID, MONITORING_LOCATION, COLLECTION_START, PARAMETER, RESULT_LETTER, RESULT, SAMPLE_CLASS, UPPER_DEPTH, LOWER_DEPTH)
+shuswap_df <- select(shuswap_clean, EMS_ID, MONITORING_LOCATION, COLLECTION_START, PARAMETER, RESULT_LETTER, RESULT, UNIT, SAMPLE_CLASS, UPPER_DEPTH, LOWER_DEPTH)
 
-## You can create a list of any column
-#parameters <- distinct(all_data, PARAMETER)
+## You can create a list of any column if you want
+#parameters <- distinct(shuswap_df, MONITORING_SITE = "", PARAMETER)
+
+##### PHOSPHORUS #####
+# Initial Visualization
+shuswap_TP <- filter(shuswap_df, PARAMETER == "Phosphorus Total")
+
+# Change units from mg/L to ug/L
+shuswap_TP <- transform(shuswap_TP, RESULT = RESULT*1000)
+colnames(shuswap_TP)[6] <- "RESULT_ugL"
+
+sites <- c("E206771", "0500124", "E208723", "0500123")
+sites_P <- filter(shuswap_TP, MONITORING_LOCATION  %in% sites)
+
+for (s in sites){
+  P_plots <- filter(sites_P, Sites == s)
+  plot <- ggplot(shuswap_TP, aes(x = COLLECTION_START, y = RESULT_ugL)) +
+  geom_point() +
+  facet_wrap(PARAMETER ~ EMS_ID, scales = "free_y")
+  plot(plot)
+}
+
+## Remove 4 rows of 8/21/2002 and 2/11/2003 that look like they were entered wrong at 100 ug/L. They are entered twice, the second entry at < MDL of 2 ug/L which makes more sense.
+##
+## Remove value of 20 (has a < so supposed to be <MDL)
+##
+## Change format of the date to remove time
+
+## Separate df into growing season (May - October) and non-growing season (November to April)
+##
+## Include surface samples only (it's just most recent data that have deep water samples)
+##
+## Average values per day
+##
+##### NITROGEN #####
+#####
+#####
+#####
+#####
+##### DISSOLVED OXYGEN #####
+#####
+#####
+#####
+##### CHLOROPHYLL A
+#####
+#####
+#####
+##### SECCHI DEPTH ######
+## CREATE CSV OF CLEAN DATA (DO THIS FOR RAW DF, AND ALL CLEANED UP PARAMETER DFs)
+write.csv(shuswap_df,
+'C:/R Projects/wqo_shuswap/data/all_data_shuswap.csv', row.names = FALSE)
+
+write.csv(shuswap_TP,
+'C:/R Projects/wqo_shuswap/data/TP_shuswap.csv', row.names = FALSE)
