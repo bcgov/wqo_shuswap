b814a0b2ecacd2d2f5f394beb441f129cabe33b4
Heather Thompson <heather.c.thompson@gov.bc.ca>
2019-01-14 14:53:21 -0800

    year column and group by year

--- R/03_analysis.R
+++ R/03_analysis.R
@@ -13,10 +13,12 @@
 
 # PLOT GROWING SEASON MEANS
 # Bind together all clean P datasets for the 4 sites
-plotbox <- ggplot(clean_TP_0500123_gs, aes(x = Station, y = RESULT_ugL)) +
+plotbox <- ggplot(subset(shuswap_TP_0500123_gs, Year>1999), aes(group = Year, x = COLLECTION_START, y = RESULT_ugL)) +
 #facet_wrap(PARAMETER ~ EMS_ID, scales = "free_y")
 geom_boxplot()
 plot(plotbox)
+
+
 
 # would be great to make box plots for various parameters (P and N) and then a bunch of scatterplots of the other parameters if needed. Or at least load the clean datasheet and then do plot code individually.
 # May be able to join all the clean data together if its in the same form and then create a function to make them all together.
