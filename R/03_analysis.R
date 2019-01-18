c833137560ac10d5136244e2dbd100e72f7a142d
Heather Thompson <heather.c.thompson@gov.bc.ca>
2019-01-17 17:17:31 -0800

    summary table all site sand started P by year

--- R/03_analysis.R
+++ R/03_analysis.R
@@ -10,17 +10,37 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-#Phosphorus Summary Table
-salmon_arm_sum <- shuswap_TP_E206771 %>%
-  #group_by(, Units, ResultLetter) %>%
+###################################### PHOSPHORUS #####################################
+# TP SUMMARY TABLE
+TP_sum_table <- TP_clean %>%
+  group_by(EMS_ID, MONITORING_LOCATION) %>%
   summarise(Min=min(RESULT_ugL_avg), Max=max(RESULT_ugL_avg), Median=median(RESULT_ugL_avg), n=length(RESULT_ugL_avg))
-print(salmon_arm_sum)
+
+sites <- c("E206771", "0500124", "E208723", "0500123")
+
+for (s in sites){
+  TP_year <- filter(TP_clean, EMS_ID == s)
+  plotTPyear <- ggplot(subset(TP_year, Year>2009), aes(x = Month, y = RESULT_ugL_avg, color = Year, group = Year)) +
+    geom_line() +
+    ggtitle(s) +
+    xlab("Month") +
+    ylab("Total Phosphorus (ug/L)")
+  plot(plotTPyear)
+}
+
+# PLOT SALMON RUN NUMBERS TO COMPARE TO TP
+thompson_fish <- read_csv("data/thompson_fish_numbers.csv")
+fish <- ggplot(subset(thompson_fish, year>1994), aes(x = year, y = number_of_fish, color = location)) +
+  geom_line() +
+  #geom_dl(aes(label = location), method = "last.points") +
+  #scale_x_date(labels = date_format("%Y"), date_breaks = "1 year") +
+  xlab("Year of Fish Run") +
+  ylab("Number of Salmon")
+plot(fish)
+ggsave(filename = "fish_thompson.png", plot = fish, path = 'C:/R Projects/wqo_shuswap/outputs', width = 9, height = 5, units= "in")
 
 # PLOT GROWING SEASON MEANS
-# Load fish data to plot with TP
-thompson_fish <- read_csv("data/thompson_fish_numbers.csv")
-
-sorrento_box <- ggplot(subset(shuswap_TP_0500123_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_ugL_avg)) +
+sorrento_box <- ggplot(subset(TP_0500123_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_ugL_avg)) +
 #facet_wrap(PARAMETER ~ EMS_ID, scales = "free_y")
 geom_boxplot() +
   geom_hline(aes(yintercept = 10), colour = "red", linetype = "dashed") +
@@ -30,7 +50,7 @@ geom_boxplot() +
   #scale_x_date(labels = date_format("%Y")) +
  plot(sorrento_box)
 
-salmon_arm_box <- ggplot(subset(shuswap_TP_E206771_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_ugL_avg)) +
+salmon_arm_box <- ggplot(subset(TP_E206771_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_ugL_avg)) +
    geom_boxplot() +
   geom_hline(aes(yintercept = 15), colour = "red", linetype = "dashed") +
    ggtitle("Salmon Arm - E206771") +
@@ -38,8 +58,7 @@ salmon_arm_box <- ggplot(subset(shuswap_TP_E206771_gs, Year>1999), aes(group = Y
    ylab("Growing Season TP (ug/L)")
 plot(salmon_arm_box)
 
-
- salmon_arm_point <- ggplot(subset(shuswap_TP_E206771_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_ugL_avg)) +
+salmon_arm_point <- ggplot(subset(TP_E206771_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_ugL_avg)) +
    geom_point() +
    geom_hline(aes(yintercept = 15), colour = "red", linetype = "dashed") +
    ggtitle("Salmon Arm - E206771") +
@@ -47,8 +66,7 @@ plot(salmon_arm_box)
    ylab("Growing Season TP (ug/L)")
  plot(salmon_arm_point)
 
-
-sicamous_box <- ggplot(subset(shuswap_TP_0500124_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_ugL_avg)) +
+sicamous_box <- ggplot(subset(TP_0500124_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_ugL_avg)) +
    geom_boxplot() +
   geom_hline(aes(yintercept = 10), colour = "red", linetype = "dashed") +
    ggtitle("Sicamous Reach - 0500124") +
@@ -56,29 +74,10 @@ sicamous_box <- ggplot(subset(shuswap_TP_0500124_gs, Year>1999), aes(group = Yea
    ylab("Growing Season TP (ug/L)")
  plot(sicamous_box)
 
- main_arm_point <- ggplot(subset(shuswap_TP_E208723_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_ugL_avg)) +
+main_arm_point <- ggplot(subset(TP_E208723_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_ugL_avg)) +
    geom_point() +
    geom_hline(aes(yintercept = 10), colour = "red", linetype = "dashed") +
    ggtitle("Main Arm - E208723") +
    xlab("Date") +
    ylab("Growing Season TP (ug/L)")
  plot(main_arm_point)
-
- fish <- ggplot(subset(thompson_fish, year>1994), aes(x = year, y = number_of_fish, color = location)) +
-   geom_line() +
-   #geom_dl(aes(label = location), method = "last.points") +
-   #scale_x_date(labels = date_format("%Y"), date_breaks = "1 year") +
-   xlab("Year of Fish Run") +
-   ylab("Number of Salmon")
-plot(fish)
-ggsave(filename = "fish_thompson.png", plot = fish, path = 'C:/R Projects/wqo_shuswap/outputs', width = 9, height = 5, units= "in")
-
-# Plot total P to figure out spring overturn P concentration
-# sorrento_line <- ggplot(subset(shuswap_TP_0500123, Year>2009), aes(x = COLLECTION_START, y = RESULT_ugL)) +
-#   geom_line() +
-#   #geom_hline(aes(yintercept = 15), colour = "red", linetype = "dashed") +
-#   ggtitle("Sorrento Reach-0500123 TP (ug/L)") +
-#   xlab("Date") +
-#   ylab("Total Phosphorus (ug/L)")
-# plot(sorrento_line)
-#
