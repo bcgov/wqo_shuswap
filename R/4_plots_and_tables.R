e07ab46df319611a50931c45c8ea96d6ccec033e
Heather Thompson <heather.c.thompson@gov.bc.ca>
2019-05-27 14:04:10 -0700

    created yearly growing season means TP line plots for the 4 sites and individual box and line plots for the 4 sites. Too specific to put them into a loop.

--- R/4_plots_and_tables.R
+++ R/4_plots_and_tables.R
@@ -23,7 +23,7 @@ TP_sum_table <- TP_gs %>%
   group_by(EMS_ID) %>%
   summarise(Min=min(RESULT_mm), Max=max(RESULT_mm), Median=median(RESULT_mm), Mean=mean(RESULT_mm), n=length(RESULT_mm))
 
-# TP line plot for each site for growing season data since 2000. Epilimion data.
+# TP line plot for each site for growing season monthly means data since 2000. Epilimion data.
 TP_year <- TP_gs %>%
   filter(UPPER_DEPTH < 11) %>%
   filter(year(COLLECTION_START) > 1999) %>%
@@ -37,46 +37,11 @@ TP_year <- TP_gs %>%
   geom_hline(yintercept = 10,linetype="dashed",color="red")
 plot(TP_year)
 
-
-
-###Plot these in a loop by grouping by EMS_ID with TP_gs df
-###
-daily_lakedata %>%
-filter(`Phosphorus Total` < 500) %>%
-  #filter((month(COLLECTION_START) <= 5))%>%
-  #filter(COLLECTION_START > "2000-01-01 10:30:00 -08") %>%
-  group_by(MONITORING_LOCATION) %>%
-  #Calculate quantiles, etc to order data by
-  mutate(med_phos = median(x = `Phosphorus Total`, na.rm = TRUE)) %>%
-  ungroup() %>%
-  ggplot(aes(x = year(COLLECTION_START), y = (`Phosphorus Total`), group = year(COLLECTION_START))) +
-  geom_boxplot(aes(fill=as.factor(year(COLLECTION_START)))) +
-  stat_summary(fun.data = give.n, geom = "text", fun.y = median)  +
-  #coord_flip() +
-  theme_minimal() +
-  scale_y_continuous(sec.axis = dup_axis()) +
-  theme(axis.text.x=element_text(angle = 0, hjust = 1), text = element_text(size = 12), legend.position = "none") +
-  labs(x="",
-       y="Total Phosphorus (ug / L)",
-       title = paste(unique(wide_rawData$MONITORING_LOCATION), ": Total Phosphorus")) +
-  geom_hline(yintercept = 30,linetype="dashed",color="red") +
-  geom_hline(yintercept = 10,linetype="dashed",color="green") +
-  geom_hline(yintercept = 1.0, linetype="dashed", color="blue")
-
-# PLOT GROWING SEASON MEANS
-# Sorrento Box Plot
-sorrento_point <- ggplot(subset(TP_0500123_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_month_mean)) +
-  #facet_wrap(PARAMETER ~ EMS_ID, scales = "free_y")
-  geom_point() +
-  geom_hline(aes(yintercept = 10), colour = "red", linetype = "dashed") +
-  ggtitle("Sorrento Reach - 0500123") +
-  xlab("Date") +
-  ylab("Growing Season TP (ug/L)")
-#scale_x_date(labels = date_format("%Y")) +
-plot(sorrento_point)
-
 # Salmon Arm Box Plot
-salmon_arm_box <- ggplot(subset(TP_E206771_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_month_mean)) +
+salmon_arm_box <- filter(TP_gs, EMS_ID == "E206771") %>%
+  filter(UPPER_DEPTH < 11) %>%
+  filter(year(COLLECTION_START) > 1999) %>%
+  ggplot(aes(group = year(COLLECTION_START), x = year(COLLECTION_START), y = RESULT_mm)) +
   geom_boxplot() +
   geom_hline(aes(yintercept = 15), colour = "red", linetype = "dashed") +
   ggtitle("Salmon Arm - E206771") +
@@ -85,7 +50,10 @@ salmon_arm_box <- ggplot(subset(TP_E206771_gs, Year>1999), aes(group = Year, x =
 plot(salmon_arm_box)
 
 # Salmon Arm Point Plot
-salmon_arm_point <- ggplot(subset(TP_E206771_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_month_mean)) +
+salmon_arm_point <- filter(TP_gs, EMS_ID == "E206771") %>%
+  filter(UPPER_DEPTH < 11) %>%
+  filter(year(COLLECTION_START) > 1999) %>%
+  ggplot(aes(group = year(COLLECTION_START), x = year(COLLECTION_START), y = RESULT_mm)) +
   geom_point() +
   geom_hline(aes(yintercept = 15), colour = "red", linetype = "dashed") +
   ggtitle("Salmon Arm - E206771") +
@@ -94,7 +62,10 @@ salmon_arm_point <- ggplot(subset(TP_E206771_gs, Year>1999), aes(group = Year, x
 plot(salmon_arm_point)
 
 # Sicamous Box Plot
-sicamous_box <- ggplot(subset(TP_0500124_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_month_mean)) +
+sicamous_box <- filter(TP_gs, EMS_ID == "0500124") %>%
+  filter(UPPER_DEPTH < 11) %>%
+  filter(year(COLLECTION_START) > 1999) %>%
+  ggplot(aes(group = year(COLLECTION_START), x = year(COLLECTION_START), y = RESULT_mm)) +
   geom_boxplot() +
   geom_hline(aes(yintercept = 10), colour = "red", linetype = "dashed") +
   ggtitle("Sicamous Reach - 0500124") +
@@ -103,7 +74,10 @@ sicamous_box <- ggplot(subset(TP_0500124_gs, Year>1999), aes(group = Year, x = Y
 plot(sicamous_box)
 
 # Main Arm Point Plot
-main_arm_point <- ggplot(subset(TP_E208723_gs, Year>1999), aes(group = Year, x = Year, y = RESULT_month_mean)) +
+main_arm_point <- filter(TP_gs, EMS_ID == "E208723") %>%
+  filter(UPPER_DEPTH < 11) %>%
+  filter(year(COLLECTION_START) > 1999) %>%
+  ggplot(aes(group = year(COLLECTION_START), x = year(COLLECTION_START), y = RESULT_mm)) +
   geom_point() +
   geom_hline(aes(yintercept = 10), colour = "red", linetype = "dashed") +
   ggtitle("Main Arm - E208723") +
